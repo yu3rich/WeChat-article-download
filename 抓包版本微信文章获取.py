55b5fcd7195d5c73543ed5460069feaa84b30064
yu3rich <107763847+yu3rich@users.noreply.github.com>
2023-06-03 18:31:22 +0800

    Update 抓包版本微信文章获取.py

--- 抓包版本微信文章获取.py
+++ 抓包版本微信文章获取.py
@@ -1,56 +1,40 @@
+import json
+import time
+
 import requests
 import re
-import store   # 文件储存模块
+import store
 from pip._vendor import requests, urllib3
-"""
-需要配合抓包工具获取相应的参数才能获取信息
-"""
+### 微信的历史文章地址：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=替换==&scene=123#wechat_redirect
+### 替换其中的biz值即可
+### biz值可在任意一篇文章中用浏览器查看审核元素
 
 # 微信公众号唯一ID
 biz = '抓包工具获取'
-# 个人微信id
+# 个人微信id（不改变的确认值）
 uin = '抓包工具获取'
 # 定时获取的验证的key
 key = '抓包工具获取'
+
 offset = 0  # 页数，0开始，每10增长
 count = '10'  # 每页显示的数量，有限制最多数
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63090217) XWEB/6939 Flue'}
 
+url = f'https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz={biz}==&f=json&offset={offset}&count={count}&is_ok=1&uin={uin}%3D%3D&key={key}'
 
-def gett(biz, uin, key, offset, count):
-    rul = f"https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz={biz}==&" \
-          f"f=json&offset={offset}&count={count}&is_ok=1&uin={uin}&" \
-          f"key={key}"
-    ttt = f'https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz={biz}==&f=json&offset={offset}&count={count}&is_ok=1&uin={uin}%3D%3D&key={key}'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # 解决证书报错
 
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # 解决证书报错
-
-    response = requests.get(ttt, headers=headers, verify=False).text  # 获取页面返回的文本
-
-    new = response.replace('/', '')  # 删除获取文章中的反斜杠
-    title_re = re.compile(r'(?<=title\\":\\").*?(?=\\",\\"digest)')  # 获取标题的正表达式
-    wxrul_re = re.compile(r'(?<=content_url\\":\\").*?(?=#wechat_redirect)')  # 获取文章链接的正表达式
-    jpgrul_re = re.compile(r'(?<=cover\\":\\").*?=jpeg')  # 获取封面图片的正表达式
-
-    title_data = title_re.findall(new)
-    wxrul_data = wxrul_re.findall(new)
-    jpgrul_data = jpgrul_re.findall(new)
-    datass = []
-    for i, o, p in zip(title_data,wxrul_data,jpgrul_data):
-        list = [i,'https://mp.weixin.qq.com/s?'+re.sub(r'http:\\mp.weixin.qq.com\\s\?|amp;','',o),p]
-        datass.append(list)
-    return datass
-
-
-store = store.Dataxls('gat公众号.xlsx',['标题', '文章链接', '图片'])
-
-while True:
-    ttt = gett(biz, uin, key, offset, count)
-    for i in ttt:
-        print(f'正在储存:{i[0]}\n')
-        store.xls_1(i)
-    if offset == 1000:
-        break
-    else:
-        offset = offset + 10
+response = requests.get(url, headers=headers, verify=False)  # 获取页面
+datalist = response.json()['general_msg_list']
+datalist_1 = json.loads(datalist)  # 转换一下格式数据
+for i in datalist_1['list']:
+    datatime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(i['comm_msg_info']['datetime']))  # 时间戳转换成正常时间
+    head_title = i['app_msg_ext_info']['title']  # 头条文章标题
+    content_url = 'https://mp.weixin.qq.com/s'+re.sub(r'http://mp.weixin.qq.com/s/?|amp;','',i['app_msg_ext_info']['content_url'])  # 头条文章链接，正表达式替换成可获取阅读量的链接
+    print(f'{datatime}\n{head_title}\n{content_url}')
+    for o in i['app_msg_ext_info']['multi_app_msg_item_list']:
+        order_title = o['title']  # 次条文章标题
+        order_content_url = 'https://mp.weixin.qq.com/s'+re.sub(r'http://mp.weixin.qq.com/s/?|amp;','',o['content_url'])  # 次条文章链接，正表达式替换成可获取阅读量的链接
+        print(f'{order_title}\n{order_content_url}')
+    print('\n\n')
